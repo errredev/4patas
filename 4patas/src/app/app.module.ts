d9e840354200cd96d4e41c8f8cbcd43a666e9b21
RodolfoLeiva <Rodolfoleivacl@gmail.com>
2020-02-20 19:25:33 -0300

    angular animation
    librerias acorde a la version 8.2

--- 4patas/src/app/app.module.ts
+++ 4patas/src/app/app.module.ts
@@ -9,13 +9,14 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { RComponentsModule } from '../app/r-component/r.component.module';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
   imports: [
     BrowserModule,
     RComponentsModule,
+    BrowserAnimationsModule,
     IonicModule.forRoot(),
     AppRoutingModule
   ],
